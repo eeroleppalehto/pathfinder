1c186e96bc7563f6e6ed71fa5c24ab8d4d1c3dc8
Slyre-git <tuoppii_1@hotmail.com>
2025-04-17 15:08:41 +0300

    documentation
    algorithms documentation

--- Algorithms/BreadthFirstSearch.py
+++ Algorithms/BreadthFirstSearch.py
@@ -11,16 +11,19 @@ def bfs_steps(maze, start, end, snapshot_interval=1):
         q.put((start, []))
         visited = set()
         final_path = []
+        endpoints = ('S', 'E')
 
+        # grab the next node and the path to that node
         while not q.empty():
             (x, y), path = q.get()
             if (x, y) in visited:
                 continue
             visited.add((x, y))
-            if original[x][y] not in ('S', 'E'):
+            if original[x][y] not in endpoints:
                 current_changes.append((x, y, 'V'))
                 original[x][y] = 'V'
             move_count += 1
+            # periodically take a snapshot of steps
             if move_count % snapshot_interval == 0:
                 if current_changes:
                     steps.append(current_changes)
@@ -28,16 +31,18 @@ def bfs_steps(maze, start, end, snapshot_interval=1):
             if (x, y) == end:
                 final_path = path + [(x, y)]
                 break
+            # add all the possible neighbouring nodes
             for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
                 nx, ny = x+dx, y+dy
                 if 0<=nx<len(original) and 0<=ny<len(original[0]) and original[nx][ny] !=1:
                     q.put(((nx, ny), path + [(x, y)]))
+        # append any changes left since last snapshot
         if current_changes:
             steps.append(current_changes)
         path_changes = []
         if final_path:
             for (x,y) in final_path:
-                if original[x][y] not in ('S', 'E'):
+                if original[x][y] not in endpoints:
                     path_changes.append((x,y,'P'))
             steps.append(path_changes)
         return final_path, steps
